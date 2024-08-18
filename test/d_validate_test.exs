fbb974cd68eb157a43407717acd837b5a9f03167
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-08-18 23:48:25 +0100

    add present test

--- test/d_validate_test.exs
+++ test/d_validate_test.exs
@@ -2,7 +2,8 @@ defmodule DValidateTest do
   use ExUnit.Case
   doctest DValidate
 
-  test "greets the world" do
-    assert DValidate.hello() == :world
+  test "validate_presence/2" do
+    assert DValidate.validate_presence("value", :field_name) == :ok
+    assert DValidate.validate_presence(nil, :field_name) == {:error, :field_name, "is required"}
   end
 end
