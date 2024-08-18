84bf488d50907cb0532cd06bb613435fb7a341b3
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-08-19 00:40:41 +0100

    update mix

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule DValidate.MixProject do
       app: :d_validate,
       version: "0.1.0",
       elixir: "~> 1.17",
+      description: "A module for validating input fields in Elixir applications.",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,6 +25,17 @@ defmodule DValidate.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Rodrigo Martins <rrmartinsjg@gmail.com>"],
+      licenses: ["MIT"],
+      links: %{
+        "Changelog" => "https://hexdocs.pm/d_validate/changelog.html",
+        "GitHub" => "https://github.com/rrmartins/d_validate"
+      }
     ]
   end
 end
