37584ee832fcd331cb51a92c2d317c63181a5a04
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-08-18 23:44:25 +0100

    first commit

---
+++ lib/d_validate.ex
@@ -0,0 +1,18 @@
+defmodule DValidate do
+  @moduledoc """
+  Documentation for `DValidate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DValidate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/d_validate_test.exs
@@ -0,0 +1,8 @@
+defmodule DValidateTest do
+  use ExUnit.Case
+  doctest DValidate
+
+  test "greets the world" do
+    assert DValidate.hello() == :world
+  end
+end
