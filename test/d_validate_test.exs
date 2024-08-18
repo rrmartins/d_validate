b8f00d710cb8768517f432bba8c7346ff6866a22
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-08-18 23:54:18 +0100

    add validate type test

--- test/d_validate_test.exs
+++ test/d_validate_test.exs
@@ -6,4 +6,11 @@ defmodule DValidateTest do
     assert DValidate.validate_presence("value", :field_name) == :ok
     assert DValidate.validate_presence(nil, :field_name) == {:error, :field_name, "is required"}
   end
+
+  test "validate_type/2" do
+    assert DValidate.validate_type("value", :string) == :ok
+    assert DValidate.validate_type(1, :integer) == :ok
+    assert DValidate.validate_type(1.0, :float) == :ok
+    assert DValidate.validate_type(123, :string) == {:error, :string, "is invalid"}
+  end
 end
