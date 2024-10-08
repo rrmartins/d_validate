c575e317535f5688dac875cbdcb85e50ba53a570
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-10-08 11:36:33 +0100

    add delegate to validatePresence

--- lib/d_validate.ex
+++ lib/d_validate.ex
@@ -3,25 +3,9 @@ defmodule DValidate do
   A module for validating input fields in Elixir applications.
   """
 
-  @doc """
-  Validates if a value is present.
+  alias DValidate.ValidatePresence
 
-  ## Examples
-
-      iex> DValidate.validate_presence("value", :field_name)
-      :ok
-
-      iex> DValidate.validate_presence(nil, :field_name)
-      {:error, :field_name, "is required"}
-
-  """
-  def validate_presence(value, field_name) do
-    if value in [nil, ""] do
-      {:error, field_name, "is required"}
-    else
-      :ok
-    end
-  end
+  defdelegate validate_presence(value, field_name), to: ValidatePresence, as: :call
 
   @doc """
   Validates if a value is of the specified type.
