e2d5f8889b4d9aec3db0a5824d5c19f1fa0cf8e7
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-10-31 01:02:35 +0000

    call defdelegate to validate type

--- lib/d_validate.ex
+++ lib/d_validate.ex
@@ -3,30 +3,10 @@ defmodule DValidate do
   A module for validating input fields in Elixir applications.
   """
 
-  alias DValidate.ValidatePresence
+  alias DValidate.{ValidatePresence, ValidateType}
 
   defdelegate validate_presence(value, field_name), to: ValidatePresence, as: :call
-
-  @doc """
-  Validates if a value is of the specified type.
-
-  ## Examples
-      iex> DValidate.validate_type(1, :integer)
-      :ok
-
-      iex> DValidate.validate_type("value", :string)
-      :ok
-
-      iex> DValidate.validate_type(1.0, :float)
-      :ok
-
-      iex> DValidate.validate_type("value", :integer)
-      {:error, :integer, "is invalid"}
-  """
-  def validate_type(value, :string) when is_binary(value), do: :ok
-  def validate_type(value, :integer) when is_integer(value), do: :ok
-  def validate_type(value, :float) when is_float(value), do: :ok
-  def validate_type(_, type), do: {:error, type, "is invalid"}
+  defdelegate validate_type(value, type), to: ValidateType, as: :call
 
   @doc """
   Validates if a value matches the given regex pattern.
